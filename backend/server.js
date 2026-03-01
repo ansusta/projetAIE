768ecaf000caba7ff4773bf2c32f580effe82779
ansusta <mytasusnaamv@gmail.com>
2026-03-01 23:28:30 +0100

    Initial project setup: React, Express, AI microservice

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -10,8 +11,24 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
+app.post("/api/match", async (req, res) => {
+  try {
+    const { cvText, jobText } = req.body;
+
+    const response = await axios.post("http://localhost:8000/match", {
+      cv_text: cvText,
+      job_text: jobText
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "AI service failed" });
+  }
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
