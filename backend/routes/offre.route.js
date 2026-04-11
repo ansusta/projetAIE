5c440074c61d2e968b6652c9f7b5af9c807fb59b
ansusta <mytasusnaamv@gmail.com>
2026-04-11 01:09:45 +0100

    i forgor to push

--- backend/routes/offre.route.js
+++ backend/routes/offre.route.js
@@ -4,12 +4,12 @@ const { creerOffre, listerOffres, mesOffres, getOffre, modifierOffre, supprimerO
   = require('../controllers/offre.controller')
 const { authenticate, authorize } = require('../middleware/auth.middleware')
 
-// public
+
+
+router.get('/mes-offres',  authenticate, authorize('recruteur'), mesOffres)
+
 router.get('/',    listerOffres)
 router.get('/:id', getOffre)
-
-// protected
-router.get('/mes-offres',  authenticate, authorize('recruteur'), mesOffres)
 router.post('/',           authenticate, authorize('recruteur'), creerOffre)
 router.put('/:id',         authenticate, authorize('recruteur'), modifierOffre)
 router.delete('/:id',      authenticate, authorize('recruteur', 'admin'), supprimerOffre)
